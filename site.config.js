bafa0aa3f4dfedbdcb20a4b19cea3ef672d625e9
Im Sumin (임수민) <116950962+ssum21@users.noreply.github.com>
2024-08-25 15:48:00 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "ssum",
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "/avatar.jpg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "Mobile Developer",
     bio: "I develop everything using node.",
     email: "ssumuss@khu.ac.kr",
